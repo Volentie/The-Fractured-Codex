daf5e46916584e7d6121b47999167513081c4135
Pukki <volentie@gmail.com>
2024-08-08 16:36:41 -0300

    🚧Improve player.gd

--- Scripts/Dynamic/player.gd
+++ Scripts/Dynamic/player.gd
@@ -1,5 +1,11 @@
 class_name Player extends CharacterBody3D
 
+const config = {
+	"gravity": 0.5,
+	"walk_power": 1.1,
+	"jump_power": 10,
+}
+
 var translation: Dictionary = {
 	"velocity": Vector3(),
 	"rotation": Vector3()
@@ -20,62 +26,69 @@ const map = [
 	"go_up"
 ]
 
-func _ready():
-	set_physics_process(true)
-	set_process_input(true)
-	set_process(true)
+func go_forward(_scale: float = 1) -> void:
+	translation["velocity"] -= VU3.z * _scale
 
-func go_forward():
-	translation["velocity"] -= VU3.z
+func go_backward(_scale: float = 1) -> void:
+	translation["velocity"] += VU3.z * _scale
 
-func go_backward():
-	translation["velocity"] += VU3.z
+func go_left(_scale: float = 1) -> void:
+	translation["velocity"] -= VU3.x * _scale
 
-func go_left():
-	translation["velocity"] -= VU3.x
-
-func go_right():
-	translation["velocity"] += VU3.x
+func go_right(_scale: float = 1) -> void:
+	translation["velocity"] += VU3.x * _scale
 	
-func go_up():
-	translation["velocity"] += VU3.y
+func go_up(_scale: float = 1) -> void:
+	translation["velocity"] += VU3.y * _scale
 
 func clear_velocity():
 	translation["velocity"] = VU3.c
+	velocity = VU3.c
 
-func update_transform(input_method: StringName, action: StringName) -> bool:
-	if input_method != &"holding":
-		Phaux.debug("Invalid input_method")
-		return false
-
-	var callO = Callable(self, action)
-	if callO.is_valid():
-		callO.call()
-	else:
-		return Phaux.error("Invalid action: " + action)
-	
+func apply_transform(move: bool) -> void:
 	velocity = translation["velocity"]
-	return true
+	if move:
+		move_and_slide()
 
 func move_and_clear() -> void:
-	move_and_slide()
+	apply_transform(true)
 	clear_velocity()
 
 func apply_gravity() -> void:
-	translation["velocity"] -= VU3.y
+	translation["velocity"] -= VU3.y * config.gravity
 	move_and_clear()
 
+func update_transform(input_method: StringName, action: StringName) -> String:
+	assert(input_method in custom_inputs.input_methods and action in map, "Invalid input_method or action")
+
+	var callO = Callable(self, action)
+	if callO.is_valid():
+		if action == "go_up":
+			if is_on_floor():
+				callO.call(config.jump_power)
+				return "jump"
+		else:
+			callO.call(config.walk_power)
+			return "walk"
+	else:
+		Phaux.error("Invalid action: " + action)
+	return "Error"
+
+
 func _process(_delta):
-	# Apply gravity
-	apply_gravity()
+	var movement_type
+	# Apply gravity if not grounded
+	if !is_on_floor():
+		apply_gravity()
 
 	# Check for input
 	for custom_action in map:
 		if Input.is_action_pressed(custom_action):
-			update_transform(&"holding", custom_action)
-	
+			movement_type = update_transform(&"holding", custom_action)
 	
 	# Check if velocity has changed to move
 	if translation["velocity"] != Vector3.ZERO:
-		move_and_clear()
-	
+		if movement_type == "walk":
+			move_and_clear()
+		elif movement_type == "jump":
+			apply_transform(true)
---
+++ Scripts/Util/math.gd
@@ -0,0 +1,2 @@
+func lerp(from:float, to:float, weight:float) -> float:
+    return from + (to - from)*weight
